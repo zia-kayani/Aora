36bf1a240a01d2d43e9637a7291eb90867dae56f
Zia <ziakayani.official@gmail.com>
2024-09-07 23:48:26 -0700

    Modify tabs layout file to add tab navigation

--- app/index.jsx
+++ app/index.jsx
@@ -3,14 +3,13 @@ import React, { useEffect } from 'react'
 import { Text, View , } from 'react-native'
 
 
-SplashScreen.preventAutoHideAsync();
 
 const App = ()=> {
 
     return (
       <View className="flex-1 items-center justify-center bg-white">
         <Text className='text-3xl font-plight'> Aora </Text>
-        <Link href='/profile' style={{color:'blue'}}>Go to profile</Link>
+        <Link href='/home' style={{color:'blue'}}>Go to Home</Link>
       </View>
     )
 }
